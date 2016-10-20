85c8e405914cbf64842f15bf16e100e4b90799d4
sajben94 <sajben94@gmail.com>
2016-10-20 16:42:40 +0200

    Oprava zakladneho pohybu

--- InterpreterGcode/InterpreterGcode.ino
+++ InterpreterGcode/InterpreterGcode.ino
@@ -8,58 +8,106 @@
 
 // Connect a stepper motor with 48 steps per revolution (7.5 degree)
 // to motor port #2 (M3 and M4)
-AF_Stepper motor(80, 2);
-AF_Stepper motor2(80, 1);
+AF_Stepper motor1(80, 1);
+AF_Stepper motor2(80, 2);
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Stepper test!");
 
-  motor.setSpeed(250);  // 10 rpm
+  motor1.setSpeed(250);  //rpm
   motor2.setSpeed(250);
 }
 
-
-void motoX(int step1, int step2){
-  int m = max(step1,step2);
-  for (int i = 1; i<m;i++){
-    if (step1>=i) {
-      motor.step(1, BACKWARD, SINGLE);
-      motor2.step(1, BACKWARD, SINGLE); 
+void motoAY(int step1, int step2){
+  if (step1>step2) {
+    for (int i = 1; i<step2;i++){
+      motor1.step(1, FORWARD, SINGLE);
+      motor2.step(1, BACKWARD, SINGLE);
+    }
+    for (int i = 1; i < (step1-step2); i++) {
+      motor2.release();
+      motor1.step(1, FORWARD, SINGLE);
+    }
+  }else if(step1<step2){
+    for (int i = 1; i<step1;i++){
+      motor1.step(1, FORWARD, SINGLE);
+      motor2.step(1, BACKWARD, SINGLE);
+    }
+    for (int i = 1; i < (step2-step1); i++) {
+      motor1.release();
+      motor2.step(1, BACKWARD, SINGLE);
     }
   }
 }
 
-void motoAX(int step1, int step2){
-  int m = max(step1,step2);
-  for (int i = 1; i<m;i++){
-    if (step1>=i) {
-      motor.step(1, FORWARD, SINGLE);
+void motoY(int step1, int step2){
+  if (step1>step2) {
+    for (int i = 1; i<step2;i++){
+      motor1.step(1, BACKWARD, SINGLE);
+      motor2.step(1, FORWARD, SINGLE);
+    }
+    for (int i = 1; i < (step1-step2); i++) {
+      motor2.release();
+      motor1.step(1, BACKWARD, SINGLE);
+    }
+  }else if(step1<step2){
+    for (int i = 1; i<step1;i++){
+      motor1.step(1, BACKWARD, SINGLE);
+      motor2.step(1, FORWARD, SINGLE);
+    }
+    for (int i = 1; i < (step2-step1); i++) {
+      motor1.release();
       motor2.step(1, FORWARD, SINGLE);
     }
   }
 }
 
+void motoAX(int step1, int step2){
+  if (step1>step2) {
+    for (int i = 1; i<step2;i++){
+      motor1.step(1, FORWARD, SINGLE);
+      motor2.step(1, FORWARD, SINGLE);
+    }
+    for (int i = 1; i < (step1-step2); i++) {
+      motor2.release();
+      motor1.step(1, FORWARD, SINGLE);
 
-void motoY(int step1, int step2){
-  int m = max(step1,step2);
-  for (int i = 1; i<m;i++){
-    if (step1>=i) {
-      motor.step(1, BACKWARD, SINGLE);
-      motor2.step(1, FORWARD, SINGLE); 
+    }
+  }else if(step1<step2){
+    for (int i = 1; i<step1;i++){
+      motor1.step(1, FORWARD, SINGLE);
+      motor2.step(1, FORWARD, SINGLE);
+    }
+    for (int i = 1; i < (step2-step1); i++) {
+      motor1.release();
+      motor2.step(1, FORWARD, SINGLE);
     }
   }
 }
 
-void motoAY(int step1, int step2){
-  int m = max(step1,step2);
-  for (int i = 1; i<m;i++){
-    if (step1>=i) {
-      motor.step(1, FORWARD, SINGLE);
+void motoX(int step1, int step2){
+  if (step1>step2) {
+    for (int i = 1; i<step2;i++){
+      motor1.step(1, BACKWARD, SINGLE);
+      motor2.step(1, BACKWARD, SINGLE);
+    }
+    for (int i = 1; i < (step1-step2); i++) {
+      motor2.release();
+      motor1.step(1, BACKWARD, SINGLE);
+    }
+  }else if(step1<step2){
+    for (int i = 1; i<step1;i++){
+      motor1.step(1, BACKWARD, SINGLE);
+      motor2.step(1, BACKWARD, SINGLE);
+    }
+    for (int i = 1; i < (step2-step1); i++) {
+      motor1.release();
       motor2.step(1, BACKWARD, SINGLE);
     }
   }
 }
+
 
 void loop() {
   Serial.println("Single coil steps");
