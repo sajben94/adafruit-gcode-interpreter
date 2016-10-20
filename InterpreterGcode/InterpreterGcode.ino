78abbe8b33aa7009488acea137ac54ffdbfb7565
sajben94 <sajben94@gmail.com>
2016-10-20 16:10:39 +0200

    nastavenia parametrov motora

--- InterpreterGcode/InterpreterGcode.ino
+++ InterpreterGcode/InterpreterGcode.ino
@@ -8,43 +8,45 @@
 
 // Connect a stepper motor with 48 steps per revolution (7.5 degree)
 // to motor port #2 (M3 and M4)
-AF_Stepper motor(200, 2);
-AF_Stepper motor2(200, 1);
+AF_Stepper motor(80, 2);
+AF_Stepper motor2(80, 1);
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Stepper test!");
 
-  motor.setSpeed(50);  // 10 rpm
-  motor2.setSpeed(50);
+  motor.setSpeed(250);  // 10 rpm
+  motor2.setSpeed(250);
+}
+
+
+void motoX(int step1, int step2){
+  int m = max(step1,step2);
+  for (int i = 1; i<m;i++){
+    if (step1>=i) {
+      motor.step(1, BACKWARD, SINGLE);
+      motor2.step(1, BACKWARD, SINGLE); 
+    }
+  }
 }
 
 void motoAX(int step1, int step2){
   int m = max(step1,step2);
   for (int i = 1; i<m;i++){
-  if (step1>=i) {motor.step(1, FORWARD, SINGLE);motor.release();}
-  if (step2>=i) {motor2.step(1, FORWARD, SINGLE);motor2.release();}
+    if (step1>=i) {
+      motor.step(1, FORWARD, SINGLE);
+      motor2.step(1, FORWARD, SINGLE);
+    }
   }
 }
 
-void motoX(int step1, int step2){
-  int m = max(step1,step2);
-  for (int i = 1; i<m;i++){
-  if (step1>=i) {motor.step(1, BACKWARD, SINGLE);motor.release();}
-  if (step2>=i) {motor2.step(1, BACKWARD, SINGLE);motor2.release();}
-  }
-}
 
 void motoY(int step1, int step2){
   int m = max(step1,step2);
   for (int i = 1; i<m;i++){
     if (step1>=i) {
       motor.step(1, BACKWARD, SINGLE);
-      motor.release();
-    }
-    if (step2>=i) {
-      motor2.step(1, FORWARD, SINGLE);
-      motor2.release();
+      motor2.step(1, FORWARD, SINGLE); 
     }
   }
 }
@@ -54,24 +56,16 @@ void motoAY(int step1, int step2){
   for (int i = 1; i<m;i++){
     if (step1>=i) {
       motor.step(1, FORWARD, SINGLE);
-      motor.release();
-    }
-    if (step2>=i) {
       motor2.step(1, BACKWARD, SINGLE);
-      motor2.release();
     }
   }
 }
 
 void loop() {
   Serial.println("Single coil steps");
-  motoX(200,200);
-  motoY(200,200);
-  motoAX(200,200);
-  motoAY(200,200);
-  motoX(200,0);
-  motoY(0,200);
-  motoAX(200,0);
-  motoAY(0,200);
+  motoY(250,250);
+  motoX(250,250);
+  motoAY(250,250);
+  motoAX(250,250);
 
 }
